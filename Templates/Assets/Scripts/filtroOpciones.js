9ea5523634fd97ccf9003638f54def7f2bd339da
Acaymo Granado Sanchez <123422268+acaOficial@users.noreply.github.com>
2024-02-20 21:00:15 +0000

    FiltroOpciones Remake (js+css+htmleditado)

---
+++ Templates/Assets/Scripts/filtroOpciones.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.querySelector('.filtro');
+    const template = document.getElementById('checkbox-template');
+  
+    for (let i = 1; i <= 3; i++) {
+      const clone = document.importNode(template.content, true);
+      const checkboxId = `cbk${i}-65`;
+  
+      clone.querySelector('label').setAttribute('for', checkboxId);
+      clone.querySelector('input').setAttribute('id', checkboxId);
+  
+      container.appendChild(clone);
+    }
+});
