2cbd17297390e91680579082f5fff2dbf12f3209
Santiago Santana Martínez <158832651+Tiago1615@users.noreply.github.com>
2024-02-23 19:38:06 +0000

    Añadida función para cargar htmls con js propio al main.js

--- Templates/Assets/Scripts/main.js
+++ Templates/Assets/Scripts/main.js
@@ -12,6 +12,34 @@ function fetchComponente(url, containerId) {
         .catch(error => console.error(`Error al cargar html -> ${error}`));
 }
 
+function fetchComponenteconJSPropio(url, containerId){
+    fetch(url)
+        .then(response => {
+            if (!response.ok){
+                throw new Error('Error al cargar el recurso: ' + response.status);
+            }
+            return response.text();
+        })
+        .then(html => {
+            document.getElementById(containerId).innerHTML = html;
+
+            const scripts = document.getElementById(containerId).getElementsByTagName('script');
+            for (let script of scripts){
+                if (script.src){
+                    fetch(script.src)
+                    .then(response => response.text())
+                    .then(scriptText => eval(scriptText))
+                    .catch(error => console.error(`Error loading script: ${error}`));
+                }
+                else{
+                    eval(script.innerText);
+                }
+            }
+        })
+        .catch(error => {
+            console.error(`Error loading HTML: ${error}`);
+        });
+}
 
 //Header Sin Login ni Register
 fetchComponente('../Header/HeaderSinLoginRegister/headerSinBuscar.html', 'header');
