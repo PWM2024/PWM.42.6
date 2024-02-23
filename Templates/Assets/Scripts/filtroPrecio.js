a4a27c702160211debb7f067c4af692671bc3443
Acaymo Granado Sanchez <123422268+acaOficial@users.noreply.github.com>
2024-02-23 19:20:04 +0000

    Fix FiltroPrecio

--- Templates/Assets/Scripts/filtroPrecio.js
+++ Templates/Assets/Scripts/filtroPrecio.js
@@ -6,8 +6,8 @@ const inputElements = document.querySelectorAll("input");
 
 
 function updateValueDisplays() {
-  minValueDisplay.style.left = `${(inputElements[0].value / 500) * 100}%`;
-  maxValueDisplay.style.left = `${(inputElements[1].value / 500) * 100}%`;
+  minValueDisplay.style.left = `${(inputElements[0].value / 1000) * 100}%`;
+  maxValueDisplay.style.left = `${(inputElements[1].value / 1000) * 100}%`;
 
   minValueDisplay.textContent = `${inputElements[0].value}€`;
   maxValueDisplay.textContent = `${inputElements[1].value}€`;
@@ -19,17 +19,17 @@ function validateRange() {
   const step = parseInt(inputElements[0].step);
 
 
-  if (maxPrice < (minPrice + 5) && ((500-maxPrice) > (minPrice-0))) {
+  if (maxPrice < (minPrice + 5) && ((1000-maxPrice) > (minPrice-0))) {
     inputElements[1].value = minPrice + step;
     return;
   }
-  if (minPrice > (maxPrice - 5) && ((500-maxPrice) < (minPrice-0))){
+  if (minPrice > (maxPrice - 5) && ((1000-maxPrice) < (minPrice-0))){
     inputElements[0].value = maxPrice - step;
     return;
   }
 
-  const minPercentage = ((minPrice - 5) / 490) * 100;
-  const maxPercentage = ((maxPrice - 5) / 490) * 100;
+  const minPercentage = ((minPrice - 5) / 990) * 100;
+  const maxPercentage = ((maxPrice - 5) / 990) * 100;
 
   rangeFill.style.left = minPercentage + "%";
   rangeFill.style.width = maxPercentage - minPercentage + "%";
