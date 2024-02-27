92af17310cc5c68742ba18a8bb6547b4c86d05b6
Acaymo Granado Sanchez <123422268+acaOficial@users.noreply.github.com>
2024-02-27 19:25:58 +0000

    Update miCuenta.js

--- Templates/Assets/Scripts/miCuenta.js
+++ Templates/Assets/Scripts/miCuenta.js
@@ -1,7 +1,7 @@
 var seleccion = document.getElementsByClassName('account');
 seleccion[0].addEventListener('change', function() {
     if (seleccion[0].value === 'opcion1'){
-        window.location.href = "/Templates/Pages/miPerfil.html";
+        window.location.href = "/Templates/Pages/perfilMisDatos.html";
     }
     else if (seleccion[0].value === 'opcion2'){
         window.location.href = "/Templates/Pages/cesta.html";
