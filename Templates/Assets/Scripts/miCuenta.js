47ad1a3a344014b691a7c01e106b16bce6807bf1
Santiago Santana Martínez <158832651+Tiago1615@users.noreply.github.com>
2024-02-27 18:58:16 +0000

    Botones mi cuenta funcionales

---
+++ Templates/Assets/Scripts/miCuenta.js
@@ -0,0 +1,12 @@
+var seleccion = document.getElementsByClassName('account');
+seleccion[0].addEventListener('change', function() {
+    if (seleccion[0].value === 'opcion1'){
+        window.location.href = "/Templates/Pages/miPerfil.html";
+    }
+    else if (seleccion[0].value === 'opcion2'){
+        window.location.href = "/Templates/Pages/cesta.html";
+    }
+    else if (seleccion[0].value === 'opcion3'){
+        window.location.href = "/Templates/Pages/inicioNotLoggedIn.html";
+    }
+});
