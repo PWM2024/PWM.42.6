db0917e338d47c39929c0c52803d02b68afabad9
Acaymo Granado Sanchez <123422268+acaOficial@users.noreply.github.com>
2024-03-04 09:36:51 +0000

    fix rutas

--- Templates/Assets/Scripts/iraTienda.js
+++ Templates/Assets/Scripts/iraTienda.js
@@ -3,11 +3,11 @@ for (var i = 0; i < boton.length; i++){
     boton[i].addEventListener('click', function() {
         let current = window.location.pathname;
         
-        if (current === '/Templates/Pages/inicioNotLoggedIn.html'){
-            window.location.href = '/Templates/Pages/tiendaNotLoggedIn.html';
+        if (current === '/PWM.42.6-main/Templates/Pages/inicioNotLoggedIn.html'){
+            window.location.href = '/PWM.42.6-main/Templates/Pages/tiendaNotLoggedIn.html';
         }
-        else if (current === '/Templates/Pages/inicioLoggedIn.html'){
-            window.location.href = '/Templates/Pages/tiendaLoggedIn.html';
+        else if (current === '/PWM.42.6-main/Templates/Pages/inicioLoggedIn.html'){
+            window.location.href = '/PWM.42.6-main/Templates/Pages/tiendaLoggedIn.html';
         }
     });
 }
