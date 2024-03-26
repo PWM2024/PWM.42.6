7c3f502eea25f87bb7028f6468fcc960a524a738
Acaymo Granado Sanchez <123422268+acaOficial@users.noreply.github.com>
2024-03-26 18:56:36 +0000

    cambio a index.html

--- Templates/Assets/Scripts/iraTienda.js
+++ Templates/Assets/Scripts/iraTienda.js
@@ -3,7 +3,7 @@ for (var i = 0; i < boton.length; i++){
     boton[i].addEventListener('click', function() {
         let current = window.location.pathname;
         
-        if (current === '/PWM.42.6-main/Templates/Pages/inicioNotLoggedIn.html'){
+        if (current === '/PWM.42.6-main/Templates/Pages/index.html'){
             window.location.href = '/PWM.42.6-main/Templates/Pages/tiendaNotLoggedIn.html';
         }
         else if (current === '/PWM.42.6-main/Templates/Pages/inicioLoggedIn.html'){
