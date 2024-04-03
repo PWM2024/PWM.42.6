f50fd8d0cce18c3a9d5d389ebdb32a211837b600
acaymo <acaimopcgc@gmail.com>
2024-04-03 16:31:24 +0100

    Rutas

--- fitByte/src/app/app.routes.ts
+++ fitByte/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { InicioPage } from './pages/inicio/inicio.component';
+import { TiendaPage } from './pages/tienda/tienda.component';
+import { CalculadorasPage } from './pages/calculadoras/calculadoras.component';
+import { DietasPage } from './pages/dietas/dietas.component';
+import { PerfilPage } from './pages/perfil/perfil.component';
+import { RutinasPage } from './pages/rutinas/rutinas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: InicioPage },
+  { path: 'tienda', component: TiendaPage },
+  { path: 'calculadoras', component: CalculadorasPage },
+  { path: 'dietas', component: DietasPage },
+  { path: 'perfil', component: PerfilPage },
+  { path: 'rutinas', component: RutinasPage }
+];
