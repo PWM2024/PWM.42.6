27b55690b4ec3bca5192c1a2c16c5df01166dfef
Santiago Santana Martínez <158832651+Tiago1615@users.noreply.github.com>
2024-04-09 18:30:07 +0100

    página tienda

--- fitByte/src/app/pages/tienda/tienda.component.ts
+++ fitByte/src/app/pages/tienda/tienda.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { TarjetaProductoComponent } from '../../components/tarjeta-producto/tarjeta-producto.component';
+import { SliderComponent } from '../../components/slider/slider.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-tienda',
   standalone: true,
-  imports: [TarjetaProductoComponent],
+  imports: [TarjetaProductoComponent, SliderComponent, FooterComponent],
   templateUrl: './tienda.component.html',
   styleUrl: './tienda.component.css'
 })
 export class TiendaPage {
-
+  productos = [
+    {id:1},
+    {id:2},
+    {id:3}
+  ]
 }
