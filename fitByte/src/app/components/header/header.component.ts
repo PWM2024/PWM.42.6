86da35b31f57a9dc58cd362b19122958ec2ec31b
Juan Antonio Hernández Rodríguez <158836150+ULPGCJuan@users.noreply.github.com>
2024-04-09 13:15:26 +0100

    header sin comprobacion login ni buscar

---
+++ fitByte/src/app/components/header/header.component.ts
@@ -0,0 +1,28 @@
+import { Component, ElementRef, Renderer2 } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css', '../component.css']
+})
+export class HeaderComponent {
+
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
+
+  showSidebar() {
+    const sidebar = this.elementRef.nativeElement.querySelector('.sidebar');
+    this.renderer.setStyle(sidebar, 'transform', 'translateX(0)');
+  }
+
+  hideSidebar() {
+    const sidebar = this.elementRef.nativeElement.querySelector('.sidebar');
+    this.renderer.setStyle(sidebar, 'transform', 'translateX(-110%)');
+  }
+
+}
