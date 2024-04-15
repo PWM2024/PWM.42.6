cf833b3367e553122eaf19d6d5315bb44029ce8b
Juan Antonio Hernández Rodríguez <158836150+ULPGCJuan@users.noreply.github.com>
2024-04-15 10:09:16 +0100

    Pagina dietas

--- fitByte/src/app/pages/dietas/dietas.component.ts
+++ fitByte/src/app/pages/dietas/dietas.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import {HeaderComponent, } from "../../components/header/header.component";
+import {TarjetageneralComponent} from "../../components/tarjetageneral/tarjetageneral.component";
+import {FooterComponent} from "../../components/footer/footer.component";
+import {TarjetaProductoComponent} from "../../components/tarjeta-producto/tarjeta-producto.component";
+import {FiltroOpcionesComponent} from "../../components/filtro-opciones/filtro-opciones.component";
 
 @Component({
   selector: 'app-dietas',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent,
+    TarjetageneralComponent,
+    FooterComponent,
+    TarjetaProductoComponent,
+    FiltroOpcionesComponent,
+  ],
   templateUrl: './dietas.component.html',
   styleUrl: './dietas.component.css'
 })
 export class DietasPage {
-
+  productos = [
+    {id:1},
+    {id:2},
+    {id:3},
+    {id:4},
+    {id:5},
+    {id:6},
+    {id:7},
+    {id:8},
+    {id:9},
+    {id:10},
+    {id:11},
+    {id:12},
+    {id:13},
+    {id:14},
+    {id:15},
+    {id:16},
+    {id:17},
+    {id:18},
+  ]
 }
