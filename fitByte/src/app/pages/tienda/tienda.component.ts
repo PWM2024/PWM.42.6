be05255331923556fb9d2c31aaa77c0f9511c9d8
Juan Antonio Hernández Rodríguez <158836150+ULPGCJuan@users.noreply.github.com>
2024-04-22 09:29:57 +0100

    Imports header

--- fitByte/src/app/pages/tienda/tienda.component.ts
+++ fitByte/src/app/pages/tienda/tienda.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { TarjetaProductoComponent } from '../../components/tarjeta-producto/tarjeta-producto.component';
 import { SliderComponent } from '../../components/slider/slider.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import {HeaderComponent} from "../../components/header/header.component";
 
 @Component({
   selector: 'app-tienda',
   standalone: true,
-  imports: [TarjetaProductoComponent, SliderComponent, FooterComponent],
+  imports: [TarjetaProductoComponent, SliderComponent, FooterComponent, HeaderComponent],
   templateUrl: './tienda.component.html',
   styleUrl: './tienda.component.css'
 })
