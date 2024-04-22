de40d68277c518de967ca576d39926dfed7f0c62
Juan Antonio Hernández Rodríguez <158836150+ULPGCJuan@users.noreply.github.com>
2024-04-22 10:07:01 +0100

    Contenedor favoritos + arreglos de un colega

--- fitByte/src/app/pages/tienda/tienda.component.ts
+++ fitByte/src/app/pages/tienda/tienda.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 import { TarjetaProductoComponent } from '../../components/tarjeta-producto/tarjeta-producto.component';
 import { SliderComponent } from '../../components/slider/slider.component';
 import { FooterComponent } from '../../components/footer/footer.component';
-<<<<<<< HEAD
-import { HeaderComponent } from '../../components/header/header.component'
-=======
-import {HeaderComponent} from "../../components/header/header.component";
->>>>>>> be05255331923556fb9d2c31aaa77c0f9511c9d8
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-tienda',
