02fed377a83e797d17ce5ea3f467120cad7e0c3b
Santiago <pimisan@gmail.com>
2024-04-22 10:33:11 +0100

    Arreglo conflicto en tienda

--- fitByte/src/app/pages/tienda/tienda.component.ts
+++ fitByte/src/app/pages/tienda/tienda.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 import { TarjetaProductoComponent } from '../../components/tarjeta-producto/tarjeta-producto.component';
 import { SliderComponent } from '../../components/slider/slider.component';
 import { FooterComponent } from '../../components/footer/footer.component';
-<<<<<<< HEAD
 import { HeaderComponent } from '../../components/header/header.component'
-=======
-import { HeaderComponent } from '../../components/header/header.component';
->>>>>>> eea9144691a84cdb58eba244fedbc9298bf4f7ad
 
 @Component({
   selector: 'app-tienda',
