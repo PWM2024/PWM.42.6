41c694379dd6288012128df76485d4aa30308731
Acaymo Granado Sánchez <acaimopcgc@gmail.com>
2024-04-23 15:03:02 +0200

    arreglos body

--- fitByte/src/app/components/mi-perfil-detalles/mi-perfil-detalles.component.ts
+++ fitByte/src/app/components/mi-perfil-detalles/mi-perfil-detalles.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'mi-perfil-detalles',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './mi-perfil-detalles.component.html',
   styleUrls: ['./mi-perfil-detalles.component.css', '../component.css']
 })
 export class MiPerfilDetallesComponent {
-
+  title = 'routing-app';
 }
